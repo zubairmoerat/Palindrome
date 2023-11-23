a2175dc6aa3f1b50da5f01f01bfe3c8d6d44f441
zubairmoerat <zubairmoeratofficial@gmail.com>
2023-11-23 14:08:03 +0200

    javaDone

---
+++ script/code.js
@@ -0,0 +1,13 @@
+function isPalindrome(string) {
+    let cleanedString = string.toLowerCase();
+    let reversedString = cleanedString.split('').reverse().join('');
+    
+    if (cleanedString === reversedString) {
+      return `${string} is a palindrome!`;
+    } else {
+      return `${string} is not a palindrome.`;
+    }
+}
+
+let result = isPalindrome("Madam");
+console.log(result);
